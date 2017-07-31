52c712826e0c454f613013201f18bae8590be615
Philip Bale <pbale95@gmail.com>
2017-07-31 17:30:47 -0400

    Work on about page

--- src/views/Pages/About/About.js
+++ src/views/Pages/About/About.js
@@ -31,6 +31,9 @@ class About extends Component {
                 <p>Every agency has its own login credentials.  If you do not have your agency credentials,
                 please contact your agency Code.gov PoC or email us at <a href="mailto:team@code.gov">team@code.gov</a>.
                 Once you have your login credentials, you can head to the tool <NavLink to={'/login'}>login page</NavLink>.</p>
+                <p>After logging in, you will be redirected to the agency admin dashboard.  The dashboard contains useful information
+                such as a high-level list of current repositories, the ability to create new repositories,
+                and quick metrics about your agency. </p>
                 <div className='center'>
                   <img className='test' src={'img/dashboard.png'} alt="Agency Dashboard"/>
                   <p><small>Agency Dashboard</small></p>
@@ -53,6 +56,9 @@ class About extends Component {
                   <img className='test' src={'img/repo-edit.png'} alt="Edit Repo Page"/>
                   <p><small>Edit Repo Page</small></p>
                 </div>
+
+                <h4>6. Assistance and Updates</h4>
+                <p>The Code.gov Agency Admin Tool was...</p>
               </div>
             </div>
           </div>
